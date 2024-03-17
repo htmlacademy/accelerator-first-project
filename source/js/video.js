63788742afa076bd982b053972dca69e848ab9ad
Лев Чернин <cherninleva@gmail.com>
2024-03-17 12:16:23 +0300

    Modifying nested functions

--- source/js/video.js
+++ source/js/video.js
@@ -1,39 +1,36 @@
-(function findVideo() {
+(function () {
   const YTIMG_REGEXP = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+
+  const parseMediaURL = (media) => {
+    const url = media.src;
+    const match = url.match(YTIMG_REGEXP);
+    return match[1];
+  };
+
+  const createIframe = (src) => {
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('allow', 'autoplay');
+    iframe.setAttribute('src', src);
+    iframe.classList.add('video__media');
+    return iframe;
+  };
 
   const setupVideo = (video) => {
     const link = video.querySelector('.video__link');
     const youtube = video.querySelector('.video__media');
     const button = video.querySelector('.video__button');
 
-    const parseMediaURL = (media) => {
-      const url = media.src;
-      const match = url.match(YTIMG_REGEXP);
-
-      return match[1];
-    };
-
-
-    const createIframe = () => {
-      const id = parseMediaURL(youtube);
-      const iframe = document.createElement('iframe');
-      const query = '?rel=0&showinfo=0&autoplay=1';
-
-      iframe.setAttribute('allowfullscreen', '');
-      iframe.setAttribute('allow', 'autoplay');
-      iframe.setAttribute('src', `https://www.youtube.com/embed/${id}${query}`);
-      iframe.classList.add('video__media');
-
-      return iframe;
-    };
-
+    const id = parseMediaURL(youtube);
+    const query = '?rel=0&showinfo=0&autoplay=1';
+    const iframeSrc = `https://www.youtube.com/embed/${id}${query}`;
 
     video.addEventListener('click', () => {
-      const iframe = createIframe();
+      const iframe = createIframe(iframeSrc);
+      video.appendChild(iframe);
 
       link.remove();
       button.remove();
-      video.appendChild(iframe);
     });
 
     link.removeAttribute('href');
@@ -43,7 +40,9 @@
   const setupVideos = () => {
     document
       .querySelectorAll('.video')
-      .forEach(setupVideo);
+      .forEach((video) => {
+        setupVideo(video);
+      });
   };
 
   setupVideos();
